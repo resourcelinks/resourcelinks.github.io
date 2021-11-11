12d9870a4a39164a3764ca247d07a41725990c1b
Chuck Konkol <ckonkol@gmail.com>
2021-11-11 12:09:26 -0600

    ck. pub

---
+++ precache-manifest.9fd7a526f405b861f3e384c6485ccb16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "20e188c5aea973a68751",
+    "url": "/static/css/main.8e11a04c.chunk.css"
+  },
+  {
+    "revision": "20e188c5aea973a68751",
+    "url": "/static/js/main.7f5df4c8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7f9d7be8c54eff708c30",
+    "url": "/static/js/2.8dcd8e73.chunk.js"
+  },
+  {
+    "revision": "495f9b25739bfa2bfeb8551b1637cd5b",
+    "url": "/index.html"
+  }
+];
